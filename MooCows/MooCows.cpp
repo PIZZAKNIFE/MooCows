b310791c9b33592151de2fb4cd8b17c907038767
Brandt Solovij <brandt.solovij@gmail.com>
2017-07-12 22:15:14 -0400

    sorting out how c++ handles recursion. initialization of functions is defenitely unlike javascript, java or C#.

--- MooCows/MooCows.cpp
+++ MooCows/MooCows.cpp
@@ -2,10 +2,46 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <locale>
 
+using namespace std;
+
+constexpr int WORD_LENGTH = 6;
+
+void promptForRecurse() {
+	string doRecurse = "";
+	cout << "Want to make another guess?(yes/no)\n";
+	getline(cin, doRecurse);
+	string recurseChoice;
+	locale loc;
+	for (string::size_type i = 0; i < doRecurse.length(); ++i) {
+		recurseChoice += tolower(doRecurse[i], loc);
+	}
+	if (recurseChoice == "yes") {
+		promptUser();
+	}
+	else {
+		cout << "Thanks for Playing\n";
+	}
+}
+
+void promptUser() {
+	string Guess = "";
+	cout << "Make a guess: ";
+	getline(cin, Guess);
+	cout << "\nYour guess was: ";
+	cout << Guess + "\n";
+	promptForRecurse();
+	return;
+}
 
 int main()
 {
-    return 0;
+	cout << "Can you guess the " << WORD_LENGTH << " letter word?" << endl;
+	promptUser();
+	return 0;
 }
+
 
